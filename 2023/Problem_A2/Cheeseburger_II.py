44507b1feb402ffd9df3203c72ba11b1b5d80c24
aamna-ansari <aamnansari29@gmail.com>
2024-09-20 14:50:19 +0500

    Problem_A2

---
+++ 2023/Problem_A2/Cheeseburger_II.py
@@ -0,0 +1,25 @@
+# Cheeseburger
+
+file = open("Input.txt", "r")
+
+def solve():
+    A, B, C = map(int,file.readline().split(" "))
+
+    # single burger buy
+
+    ans_count = C // A
+
+    # Double Burger Buy
+
+    ans_count = max (ans_count, 2 * (C // B - 1))
+
+    # 1 Single + Double Remining 
+    ans_count = max(ans_count, 2 * ((C - A )// B + 1))
+
+
+    T = int(file.readline())
+
+    print("Test Case:", T)
+
+    for i in range(1, T + 1):
+        print("Case #" + 1 + ":" + solve())
